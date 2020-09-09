e166c34a6f161cd4f4e2d13c0023aa7167e17389
dominicruot <57857349+dominicruot@users.noreply.github.com>
2020-09-09 19:09:30 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+ $(".book6").on('click', function() {
+            window.location = "https://www.amazon.com/Hazards-Nation-Building-Nurturing-Competing/dp/064825917X";
+
+        });
+        $(".book7").on('click', function() {
+            window.location = "https://www.amazon.com/Idea-South-Sudan-History-Political/dp/0648259110"
+        })
+        $(".book5").on('click', function() {
+            window.location = "https://www.amazon.com/Revolution-Equatoria-Mountains-Story-Mutiny/dp/0648259137"
+        })
+       
